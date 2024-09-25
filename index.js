973460eec5dcda40b6a78c60b2f4544f4b092d98
Charlie <160505188+oneandonlycharlie@users.noreply.github.com>
2024-09-26 00:52:56 +0800

    Feat: plugin weather api to fetch data

---
+++ index.js
@@ -0,0 +1,59 @@
+// Fetch data
+
+async function getWeatherData() {
+    try {
+        const response = await fetch(
+            'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Shanghai/next7days?unitGroup=metric&include=fcst%2Cstatsfcst%2Cstats%2Cdays%2Ccurrent&key=K9DBXADCCF4V6TWJPWSSWFCBN&contentType=json',
+            {mode:'cors'});
+        const weatherData = await response.json();
+        matchToday(weatherData);
+
+        return weatherData
+    } catch(error) {
+
+    }
+}
+
+
+const matchToday = function (data) {
+    city.textContent = data["address"];
+    tempatureToday.textContent = data["days"][0]["temp"];
+    descriptionToday.textContent = data["description"];
+    condition.textContent = data["days"][0]["conditions"];
+}
+
+
+
+
+
+
+
+
+// Build a today object
+
+
+
+
+
+
+
+
+
+// build a forecast object
+
+
+
+
+
+
+
+
+
+
+
+
+// Set display control
+const city = document.querySelector(".location")
+const tempatureToday = document.querySelector(".temp")
+const descriptionToday = document.querySelector(".description")
+const condition = document.querySelector(".condition")
